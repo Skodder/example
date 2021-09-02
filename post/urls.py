7e015f57b82e9981a7b939f3127a38961d072df3
Skodder <eg.cherepanov@yandex.ru>
2021-09-02 16:45:22 +0300

    feat: add filter

--- post/urls.py
+++ post/urls.py
@@ -3,5 +3,5 @@ from django.contrib import admin
 from post.views import PostListView
 
 urlpatterns = [
-    url(r"^posts/", PostListView.as_view())
+    url(r"^posts/", PostListView.as_view(), name="post_list")
 ]
